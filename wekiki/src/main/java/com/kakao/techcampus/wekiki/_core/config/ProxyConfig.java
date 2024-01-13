e936725c3dcdefcafcf51c6a8d5bff20b77b8c74
kimdonwoo <ehstjstod@gmail.com>
2024-01-13 21:30:03 +0900

    setting : restTemplate 빈 등록 config에 @Profile 설정
    - krampoline에 배포시 프록시 서버에 요청을 위한 restTemplate 빈 등록 config에 @Profile 설정

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/config/ProxyConfig.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/config/ProxyConfig.java
@@ -1,5 +1,6 @@
 package com.kakao.techcampus.wekiki._core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,13 +10,16 @@ import org.springframework.web.client.RestTemplate;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 
+@Slf4j
 @Configuration
 public class ProxyConfig {
     private static final String PROXY_HOST = "krmp-proxy.9rum.cc";
     private static final int PROXY_PORT = 3128;
 
     @Bean
+    @Profile("prodkrampoline")
     public RestTemplate restTemplate() {
+        log.info("krampoline 배포용 프록시 서버");
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(PROXY_HOST, PROXY_PORT));
         requestFactory.setProxy(proxy);

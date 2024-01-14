5edebfde0b769f736eabb4ba5e9879183428c491
kimdonwoo <ehstjstod@gmail.com>
2024-01-14 21:47:09 +0900

    feat : Validation 유효성 검사를 위한 ExceptionHandler 등록
    - @RequestBody 요청 값 검증에서 에러 처리를 위한  @ExceptionHandler(MethodArgumentNotValidException.class) 등록
    - @PathVariable, @RequestParam 값 검증을 위한  @ExceptionHandler(ConstraintViolationException.class) 등록

--- wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/error/GlobalExceptionHandler.java
+++ wekiki/src/main/java/com/kakao/techcampus/wekiki/_core/error/GlobalExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.kakao.techcampus.wekiki._core.error;
 import com.kakao.techcampus.wekiki._core.error.exception.*;
+import com.kakao.techcampus.wekiki._core.utils.ApiUtils;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -27,4 +32,16 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(e.body(), e.status());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> MethodArgumentNotValidError(MethodArgumentNotValidException e){
+        String errorMessage = "Validation error: " + e.getBindingResult().getFieldError().getDefaultMessage();
+        return new ResponseEntity<>(ApiUtils.error(errorMessage, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> ConstraintViolationError(ConstraintViolationException e){
+        return new ResponseEntity<>(ApiUtils.error(e.getMessage(), HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
+    }
 }

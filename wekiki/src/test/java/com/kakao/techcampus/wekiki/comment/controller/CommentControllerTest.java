f451cdb33478837f1c0bb4998a5cefd13f8cd10d
kimdonwoo <ehstjstod@gmail.com>
2024-02-04 19:41:17 +0900

    test : CommentController 소형 테스트
    - @WebMvcTest와 mockmvc를 사용하지 않고 Controller 소형 테스트 진행
    - TestContainer 클래스 생성

---
+++ wekiki/src/test/java/com/kakao/techcampus/wekiki/comment/controller/CommentControllerTest.java
@@ -0,0 +1,110 @@
+package com.kakao.techcampus.wekiki.comment.controller;
+
+import com.kakao.techcampus.wekiki._core.utils.ApiUtils;
+import com.kakao.techcampus.wekiki.comment.controller.request.CommentRequest;
+import com.kakao.techcampus.wekiki.comment.controller.response.CommentResponse;
+import com.kakao.techcampus.wekiki.mock.TestContainer;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommentControllerTest {
+
+    /**
+     * 1. 사용자는_올바른_요청에_대해_댓글생성이_가능하다
+     *
+     * 2. 사용자는_올바른_요청에_대해_댓글조회가_가능하다
+     *
+     * 3. 사용자는_올바른_요청에_대해_댓글삭제가_가능하다
+     *
+     * 4. 사용자는_올바른_요청에_대해_댓글수정이_가능하다
+     */
+
+    @Test
+    void 사용자는_올바른_요청에_대해_댓글생성이_가능하다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .build();
+        testContainer.testCommentControllerSetting();
+        Long groupId = 1L;
+        Long postId = 1L;
+        CommentRequest.createComment request = new CommentRequest.createComment("create Comment Controller Test");
+
+        // when
+        ResponseEntity<ApiUtils.ApiResult<CommentResponse.createCommentDTO>> result = testContainer.commentController.createComment(groupId, postId, request);
+
+        //then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody().getResponse().getCommentId()).isEqualTo(3L);
+        assertThat(result.getBody().getResponse().getContent()).isEqualTo("create Comment Controller Test");
+        assertThat(result.getBody().getResponse().getNickName()).isEqualTo("TestMember1의 groupMember");
+    }
+
+    @Test
+    void 사용자는_올바른_요청에_대해_댓글조회가_가능하다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .build();
+        testContainer.testCommentControllerSetting();
+        Long groupId = 1L;
+        Long postId = 1L;
+        int page= 1;
+
+        // when
+        ResponseEntity<ApiUtils.ApiResult<CommentResponse.getCommentDTO>> result = testContainer.commentController.getComment(groupId, postId, page);
+
+        //then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody().getResponse().getPostId()).isEqualTo(1L);
+        assertThat(result.getBody().getResponse().getComments().get(0).getCommentId()).isEqualTo(1L);
+        assertThat(result.getBody().getResponse().getComments().get(0).getNickName()).isEqualTo("TestMember1의 groupMember");
+        assertThat(result.getBody().getResponse().getComments().get(0).getContent()).isEqualTo("TestMember1의 Test Comment1 입니다.");
+        assertThat(result.getBody().getResponse().getComments().get(1).getCommentId()).isEqualTo(2L);
+        assertThat(result.getBody().getResponse().getComments().get(1).getNickName()).isEqualTo("TestMember1의 groupMember");
+        assertThat(result.getBody().getResponse().getComments().get(1).getContent()).isEqualTo("TestMember1의 Test Comment2 입니다.");
+
+    }
+
+    @Test
+    void 사용자는_올바른_요청에_대해_댓글삭제가_가능하다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .build();
+        testContainer.testCommentControllerSetting();
+        Long groupId = 1L;
+        Long commentId = 2L;
+
+        // when
+        ResponseEntity<ApiUtils.ApiResult<CommentResponse.deleteCommentDTO>> result = testContainer.commentController.deleteComment(groupId, commentId);
+
+        //then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody().getResponse().getCommentId()).isEqualTo(2L);
+        assertThat(result.getBody().getResponse().getContent()).isEqualTo("TestMember1의 Test Comment2 입니다.");
+    }
+
+    @Test
+    void 사용자는_올바른_요청에_대해_댓글수정이_가능하다() {
+        // given
+        TestContainer testContainer = TestContainer.builder()
+                .build();
+        testContainer.testCommentControllerSetting();
+        Long groupId = 1L;
+        Long commentId = 2L;
+        CommentRequest.updateComment request = new CommentRequest.updateComment("update Comment Controller Test");
+
+        // when
+        ResponseEntity<ApiUtils.ApiResult<CommentResponse.updateCommentDTO>> result = testContainer.commentController.updateComment(groupId, commentId, request);
+
+        //then
+        assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(result.getBody().getResponse().getCommentId()).isEqualTo(2L);
+        assertThat(result.getBody().getResponse().getNewContent()).isEqualTo("update Comment Controller Test");
+    }
+
+
+
+}
---
+++ wekiki/src/test/java/com/kakao/techcampus/wekiki/mock/TestContainer.java
@@ -0,0 +1,240 @@
+package com.kakao.techcampus.wekiki.mock;
+
+import com.kakao.techcampus.wekiki._core.utils.port.RedisUtils;
+import com.kakao.techcampus.wekiki._core.utils.port.SecurityUtils;
+import com.kakao.techcampus.wekiki.comment.controller.CommentController;
+import com.kakao.techcampus.wekiki.comment.controller.port.CommentCreateService;
+import com.kakao.techcampus.wekiki.comment.controller.port.CommentDeleteService;
+import com.kakao.techcampus.wekiki.comment.controller.port.CommentReadService;
+import com.kakao.techcampus.wekiki.comment.controller.port.CommentUpdateService;
+import com.kakao.techcampus.wekiki.comment.domain.Comment;
+import com.kakao.techcampus.wekiki.comment.service.CommentServiceImpl;
+import com.kakao.techcampus.wekiki.comment.service.port.CommentRepository;
+import com.kakao.techcampus.wekiki.group.domain.Group;
+import com.kakao.techcampus.wekiki.group.domain.GroupMember;
+import com.kakao.techcampus.wekiki.group.service.port.GroupMemberRepository;
+import com.kakao.techcampus.wekiki.group.service.port.GroupRepository;
+import com.kakao.techcampus.wekiki.history.service.port.HistoryRepository;
+import com.kakao.techcampus.wekiki.member.domain.Member;
+import com.kakao.techcampus.wekiki.member.infrastructure.Authority;
+import com.kakao.techcampus.wekiki.member.service.port.MemberRepository;
+import com.kakao.techcampus.wekiki.pageInfo.controller.PageRestController;
+import com.kakao.techcampus.wekiki.pageInfo.controller.port.PageInfoCreateService;
+import com.kakao.techcampus.wekiki.pageInfo.controller.port.PageInfoDeleteService;
+import com.kakao.techcampus.wekiki.pageInfo.controller.port.PageInfoReadService;
+import com.kakao.techcampus.wekiki.pageInfo.controller.port.PageInfoUpdateService;
+import com.kakao.techcampus.wekiki.pageInfo.domain.PageInfo;
+import com.kakao.techcampus.wekiki.pageInfo.infrastructure.PageIndexGeneratorImpl;
+import com.kakao.techcampus.wekiki.pageInfo.service.PageServiceImpl;
+import com.kakao.techcampus.wekiki.pageInfo.service.port.PageIndexGenerator;
+import com.kakao.techcampus.wekiki.pageInfo.service.port.PageRepository;
+import com.kakao.techcampus.wekiki.post.controller.PostRestController;
+import com.kakao.techcampus.wekiki.post.controller.port.PostCreateService;
+import com.kakao.techcampus.wekiki.post.controller.port.PostDeleteService;
+import com.kakao.techcampus.wekiki.post.controller.port.PostReadService;
+import com.kakao.techcampus.wekiki.post.controller.port.PostUpdateService;
+import com.kakao.techcampus.wekiki.post.domain.Post;
+import com.kakao.techcampus.wekiki.post.service.PostServiceImpl;
+import com.kakao.techcampus.wekiki.post.service.port.PostRepository;
+import com.kakao.techcampus.wekiki.report.service.port.ReportRepository;
+import lombok.Builder;
+import org.redisson.api.RedissonClient;
+
+import java.time.LocalDateTime;
+
+
+public class TestContainer {
+
+    public final PageRestController pageRestController;
+    public final PageInfoCreateService pageInfoCreateService;
+    public final PageInfoDeleteService pageInfoDeleteService;
+    public final PageInfoReadService pageInfoReadService;
+    public final PageInfoUpdateService pageInfoUpdateService;
+    public final PageRepository pageRepository;
+    public final PostRepository postRepository;
+    public final PageIndexGenerator pageIndexGenerator;
+    public final SecurityUtils securityUtils;
+    public final RedisUtils redisUtils;
+    public final MemberRepository memberRepository;
+    public final GroupMemberRepository groupMemberRepository;
+    public final GroupRepository groupRepository;
+
+    //public final RedissonLockFacade redissonLockFacade;
+    //private final RedissonClient redissonClient;
+
+    public final PostRestController postRestController;
+    public final PostReadService postReadService;
+    public final PostCreateService postCreateService;
+    public final PostUpdateService postUpdateService;
+    public final PostDeleteService postDeleteService;
+    public final HistoryRepository historyRepository;
+    public final ReportRepository reportRepository;
+
+    public final CommentController commentController;
+    private final CommentReadService commentReadService;
+    private final CommentCreateService commentCreateService;
+    private final CommentUpdateService commentUpdateService;
+    private final CommentDeleteService commentDeleteService;
+
+    private final CommentRepository commentRepository;
+    private final LocalDateTime testTime = LocalDateTime.now();
+
+    @Builder
+    public TestContainer(){
+        this.pageRepository = new FakePageInfoRepository();
+        this.postRepository = new FakePostRepository();
+        this.pageIndexGenerator = new PageIndexGeneratorImpl();
+        this.redisUtils = new FakeRedisUtils();
+        this.memberRepository = new FakeMemberRepository();
+        this.groupMemberRepository = new FakeGroupMemberRepository();
+        this.groupRepository = new FakeGroupRepository();
+        this.historyRepository = new FakeHistoryRepository();
+        this.reportRepository = new FakeReportRepository();
+        this.commentRepository = new FakeCommentRepository();
+        this.securityUtils = new FakeSecurityUtils();
+
+        PageServiceImpl pageService = PageServiceImpl.builder()
+                .pageRepository(this.pageRepository)
+                .postRepository(this.postRepository)
+                .groupMemberRepository(this.groupMemberRepository)
+                .pageIndexGenerator(this.pageIndexGenerator)
+                .redisUtils(this.redisUtils)
+                .memberRepository(this.memberRepository)
+                .groupMemberRepository(this.groupMemberRepository)
+                .groupRepository(this.groupRepository)
+                .build();
+        this.pageInfoCreateService = pageService;
+        this.pageInfoDeleteService = pageService;
+        this.pageInfoReadService = pageService;
+        this.pageInfoUpdateService = pageService;
+
+        PostServiceImpl postService = PostServiceImpl.builder()
+                .pageRepository(this.pageRepository)
+                .postRepository(this.postRepository)
+                .historyRepository(this.historyRepository)
+                .reportRepository(this.reportRepository)
+                .groupMemberJPARepository(this.groupMemberRepository)
+                .build();
+        this.postReadService = postService;
+        this.postCreateService = postService;
+        this.postUpdateService = postService;
+        this.postDeleteService = postService;
+
+        CommentServiceImpl commentService = CommentServiceImpl.builder()
+                .commentRepository(this.commentRepository)
+                .postRepository(this.postRepository)
+                .groupMemberRepository(this.groupMemberRepository)
+                .build();
+        this.commentReadService = commentService;
+        this.commentCreateService = commentService;
+        this.commentUpdateService = commentService;
+        this.commentDeleteService = commentService;
+
+        this.pageRestController = PageRestController.builder()
+                .pageInfoCreateService(this.pageInfoCreateService)
+                .pageInfoDeleteService(this.pageInfoDeleteService)
+                .pageInfoReadService(this.pageInfoReadService)
+                .pageInfoUpdateService(this.pageInfoUpdateService)
+                .securityUtils(this.securityUtils)
+                .build();
+        this.postRestController = PostRestController.builder()
+                .postReadService(this.postReadService)
+                .postCreateService(this.postCreateService)
+                .postDeleteService(this.postDeleteService)
+                .postUpdateService(this.postUpdateService)
+                .securityUtils(this.securityUtils)
+                .build();
+        this.commentController = CommentController.builder()
+                .commentCreateService(this.commentCreateService)
+                .commentDeleteService(this.commentDeleteService)
+                .commentReadService(this.commentReadService)
+                .commentUpdateService(this.commentUpdateService)
+                .securityUtils(this.securityUtils)
+                .build();
+
+    }
+
+    // CommentController 테스트를 위한
+    // member, group , GroupMember, Page, Post, Comment 한개 씩 생성
+    public void testCommentControllerSetting(){
+
+        Member member1 = Member.builder()
+                .id(1L)
+                .name("TestMember1")
+                .email("test1@naver.com")
+                .password("1111")
+                .created_at( testTime)
+                //.groupMembers(groupMemberEntities.stream().map(GroupMemberEntity::toModel).toList())
+                .authority(Authority.user)
+                .build();
+
+        Group group = Group.builder()
+                .id(1L)
+                .groupName("TestGroup")
+                .groupProfileImage("s3/url")
+                //.groupMembers(this.groupMemberEntities.stream().map(GroupMemberEntity::toModel).toList())
+                .memberCount(0)
+                .created_at( testTime)
+                .build();
+
+        GroupMember groupMember1 = GroupMember.builder()
+                .member(member1)
+                .group(group)
+                .nickName("TestMember1의 groupMember")
+                .memberLevel(0)
+                .created_at( testTime)
+                .activeStatus(true)
+                .build();
+
+        GroupMember savedGroupMember1 = groupMemberRepository.save(groupMember1);
+
+        PageInfo pageInfo = PageInfo.builder()
+                .group(group)
+                .pageName("Test Page")
+                //.posts(postEntities.stream().map(PostEntity::toModel).toList())
+                .goodCount(0)
+                .badCount(0)
+                .viewCount(0)
+                .created_at( testTime)
+                .updated_at( testTime)
+                .build();
+
+        PageInfo savedPageInfo1 = pageRepository.save(pageInfo);
+
+        Post post = Post.builder()
+                //.parent(parent.toModel())
+                .orders(0)
+                .groupMember(savedGroupMember1)
+                .pageInfo(savedPageInfo1)
+                //.historys(historyEntities.stream().map(HistoryEntity::toModel).toList())
+                //.comments(commentEntities.stream().map(CommentEntity::toModel).toList())
+                .title("Test Post")
+                .content("해당 포스트는 테스트용 포스트입니다.")
+                .created_at( testTime)
+                .build();
+
+        Post savedPost = postRepository.save(post);
+
+        Comment comment1 = Comment.builder()
+                .groupMember(savedGroupMember1)
+                .post(savedPost)
+                .content("TestMember1의 Test Comment1 입니다.")
+                .created_at( testTime)
+                .build();
+
+        commentRepository.save(comment1);
+
+        Comment comment2 = Comment.builder()
+                .groupMember(savedGroupMember1)
+                .post(savedPost)
+                .content("TestMember1의 Test Comment2 입니다.")
+                .created_at( testTime)
+                .build();
+
+        commentRepository.save(comment2);
+
+
+    }
+
+
+}
